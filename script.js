b210db2b495e6e8701b84e79c4c450c58e5715fb
Steevenbr <175425597+Steevenbr@users.noreply.github.com>
2026-09-24 19:50:32 -0500

    Estructura inicial y estilos del portafolio

---
+++ script.js
@@ -0,0 +1,15 @@
+document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop - 70, // Ajuste para que el header fijo no tape el título
+                behavior: 'smooth'
+            });
+        }
+    });
+});
